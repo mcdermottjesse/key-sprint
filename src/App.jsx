0ae93455c49a2d3b50e4765ec49291c854b9c41e
Jesse McDermott <mcdermott.jess9@gmail.com>
2024-05-05 16:53:08 -0700

    Add a little more refactoring

--- src/App.jsx
+++ src/App.jsx
@@ -5,8 +5,6 @@ export default function App() {
   const [typedKey, setTypedKey] = useState("");
   const [trackKeyIndex, setTrackKeyIndex] = useState(1);
   const [errorIndexes, setErrorIndexes] = useState([]);
-  // Not needed, as wordIndex replaces what this does.
-  const [wordErrorArray, setWordErrorArray] = useState([]);
   const [spaceErrorIndexes, setSpaceErrorIndexes] = useState([]);
   const [sentence, setSentence] = useState(
     "the curious cat quietly explores the mysterious garden at every flower and chasing butterflies in the warm sunlight   "
@@ -14,8 +12,7 @@ export default function App() {
   const [correctWordCount, setCorrectWordCount] = useState(0);
   const [allWordCount, setAllWordCount] = useState(0);
   const [trackKeyCount, setTrackKeyCount] = useState([0]);
-  // Rename as this indexes incorrect spelt words.
-  const [wordIndex, setWordIndex] = useState([]);
+  const [wordErrorIndex, setWordErrorIndex] = useState([]);
 
   const nextLetterIndex = typedKey.length;
   const currentLetter = sentence[nextLetterIndex];
@@ -23,6 +20,7 @@ export default function App() {
   const sentenceArray = sentence.split(" ").filter(Boolean); // Removes empty strings from array.
   const previousLetter = sentence[nextLetterIndex - 1];
   const currentWord = sentenceArray[allWordCount];
+  const noErrorInWord = !wordErrorIndex.includes(allWordCount);
 
   useEffect(() => {
     document.addEventListener("keydown", handleKeyPress);
@@ -30,29 +28,23 @@ export default function App() {
     return () => {
       document.removeEventListener("keydown", handleKeyPress);
     };
-  }, [typedKey, correctWordCount, trackKeyIndex, allWordCount, trackKeyCount]);
+  }, [trackKeyIndex]);
 
   const handleKeyPress = (event) => {
     // Handle Shift and Tab. Display message if caps lock.
-
     if (currentLetter === " " && event.key === " ") {
       endOfWord();
     }
     if (event.key === currentLetter) {
       // Counts the correctly typed words.
-      handleCorrectKey(currentLetter, currentWord);
+      handleCorrectKey();
     } else if (event.key === "Backspace") {
-      handleBackspace(
-        nextLetterIndex,
-        lastErrorIndex,
-        previousLetter,
-        currentLetter
-      );
+      handleBackspace();
     } else if (currentLetter === " " && event.key !== " ") {
       // If a letter is typed instead of a space, display the letter.
-      handleSpaceError(event.key, nextLetterIndex, currentWord);
+      handleSpaceError(event.key);
     } else {
-      handleIncorrectLetter(currentLetter, nextLetterIndex, currentWord);
+      handleIncorrectLetter();
     }
   };
 
@@ -65,51 +57,33 @@ export default function App() {
     setTrackKeyIndex(0);
   };
 
-  const handleCorrectKey = (currentLetter, currentWord) => {
+  const handleCorrectKey = () => {
     setTypedKey((prevTypedKey) => prevTypedKey + currentLetter);
 
     setTrackKeyIndex((prevTrackKeyIndex) => prevTrackKeyIndex + 1);
 
-    if (currentLetter !== " " && !wordIndex.includes(allWordCount)) {
-      if (wordErrorArray.length > 0 && currentWord.length === trackKeyIndex) {
-        setCorrectWordCount((prevCorrectWordCount) => prevCorrectWordCount + 1);
-      }
-      if (wordErrorArray.length === 0 && currentWord.length === trackKeyIndex) {
-        setCorrectWordCount((prevCorrectWordCount) => prevCorrectWordCount + 1);
-      }
+    if (currentWord.length === trackKeyIndex && noErrorInWord) {
+      setCorrectWordCount((prevCorrectWordCount) => prevCorrectWordCount + 1);
     }
   };
 
-  function handleBackspace(
-    nextLetterIndex,
-    lastErrorIndex,
-    previousLetter,
-    currentLetter
-  ) {
+  const handleBackspace = () => {
     setTypedKey((prevTypedKey) => prevTypedKey.slice(0, -1));
 
     // Ensure trackKeyIndex does not go below one.
-    setTrackKeyIndex((prevTrackKeyIndex) => Math.max(1, prevTrackKeyIndex - 1));
-
     if (trackKeyIndex === 1) {
       setTrackKeyIndex(Math.max(1, trackKeyCount[allWordCount]));
+    } else {
+      setTrackKeyIndex((prevTrackKeyIndex) =>
+        Math.max(1, prevTrackKeyIndex - 1)
+      );
     }
 
     if (nextLetterIndex - 1 === lastErrorIndex) {
       setErrorIndexes((prevErrorIndexes) => prevErrorIndexes.slice(0, -1));
-      setWordErrorArray((prevWordErrorArray) =>
-        prevWordErrorArray.slice(0, -1)
+      setWordErrorIndex((prevWordErrorIndex) =>
+        prevWordErrorIndex.slice(0, -1)
       );
-
-      setWordIndex((prevWordIndex) => prevWordIndex.slice(0, -1));
-
-      const fliterWordIndex = wordIndex.filter(
-        (index) => index === allWordCount
-      );
-
-      if (previousLetter === " " && fliterWordIndex.length === 1) {
-        setCorrectWordCount((prevCorrectWordCount) => prevCorrectWordCount + 1);
-      }
 
       if (previousLetter === " ") {
         setSentence(
@@ -117,6 +91,16 @@ export default function App() {
             prevSentence.slice(0, nextLetterIndex - 1) +
             prevSentence.slice(nextLetterIndex)
         );
+
+        const fliterWordErrorIndex = wordErrorIndex.filter(
+          (index) => index === allWordCount
+        );
+
+        if (fliterWordErrorIndex.length === 1) {
+          setCorrectWordCount(
+            (prevCorrectWordCount) => prevCorrectWordCount + 1
+          );
+        }
       }
 
       return;
@@ -124,16 +108,15 @@ export default function App() {
 
     if (previousLetter === " ") {
       setAllWordCount((prevAllWordCount) => prevAllWordCount - 1);
-      // Ensure the trackKeyCount applies to the current word.
       setTrackKeyCount((prevTrackKeyCount) => prevTrackKeyCount.slice(0, -1));
     }
 
-    if (currentLetter === " " && !wordIndex.includes(allWordCount)) {
+    if (currentLetter === " " && noErrorInWord) {
       setCorrectWordCount((prevCorrectWordCount) => prevCorrectWordCount - 1);
     }
-  }
+  };
 
-  function handleSpaceError(incorrectTypedKey, nextLetterIndex, currentWord) {
+  const handleSpaceError = (incorrectTypedKey) => {
     setTypedKey((prevTypedKey) => prevTypedKey + incorrectTypedKey);
 
     const updatedSentence =
@@ -153,37 +136,32 @@ export default function App() {
       nextLetterIndex,
     ]);
 
-    if (correctWordCount > 0 && !wordIndex.includes(allWordCount)) {
+    if (correctWordCount > 0 && noErrorInWord) {
       setCorrectWordCount((prevCorrectWordCount) => prevCorrectWordCount - 1);
     }
 
-    setWordIndex((prevWordIndex) => [...prevWordIndex, allWordCount]);
+    setWordErrorIndex((prevWordErrorIndex) => [
+      ...prevWordErrorIndex,
+      allWordCount,
+    ]);
 
     setTrackKeyIndex((prevTrackKeyIndex) => prevTrackKeyIndex + 1);
+  };
 
-    setWordErrorArray((prevWordErrorArray) => [
-      ...prevWordErrorArray,
-      currentWord,
-    ]);
-  }
-
-  function handleIncorrectLetter(currentLetter, nextLetterIndex, currentWord) {
+  const handleIncorrectLetter = () => {
     setTypedKey((prevTypedKey) => prevTypedKey + currentLetter);
     setErrorIndexes((prevErrorIndexes) => [
       ...prevErrorIndexes,
       nextLetterIndex,
     ]);
 
-    setWordIndex((prevWordIndex) => [...prevWordIndex, allWordCount]);
+    setWordErrorIndex((prevWordErrorIndex) => [
+      ...prevWordErrorIndex,
+      allWordCount,
+    ]);
 
     setTrackKeyIndex((prevTrackKeyIndex) => prevTrackKeyIndex + 1);
-
-    // Set the wordErrorArray to contain all the incorrect spelt words.
-    setWordErrorArray((prevWordErrorArray) => [
-      ...prevWordErrorArray,
-      currentWord,
-    ]);
-  }
+  };
 
   return (
     <div className="page">
