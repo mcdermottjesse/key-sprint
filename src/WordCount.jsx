f149674049caf10f7179735452046e1ca822ba93
Jesse McDermott <mcdermott.jess9@gmail.com>
2024-05-06 15:41:27 -0700

    Handle closing the word count pop up
    When the word count pop up is closed the test restarts.

--- src/WordCount.jsx
+++ src/WordCount.jsx
@@ -1,7 +1,21 @@
-export default function WordCount({ count }) {
+import React, { useState } from "react";
+export default function WordCount({ count, onClose }) {
+  const [close, setClose] = useState(false);
+
+  const handlePopUp = () => {
+    setClose(true);
+    onClose();
+  };
+
   return (
-    <div className="popup">
-      <div className="word-counter">Your WPM: {count}</div>
-    </div>
+    !close && (
+      <div className="popup">
+        <div className="word-counter">{count} WPM</div>
+        <button className="button" onClick={handlePopUp}>
+          <img className="reset-icon" src="/arrows-rotate.svg" alt="reset" />
+        </button>
+        <div className="hover-text">reset</div>
+      </div>
+    )
   );
 }
