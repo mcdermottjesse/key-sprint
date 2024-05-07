df9ca4b188012a74e42c00d3cef70b760e2ae495
Jesse McDermott <mcdermott.jess9@gmail.com>
2024-05-07 12:56:11 -0700

    Handle senetence on test reset
    When the test is reset ensure a new sentence is generated.

--- src/App.jsx
+++ src/App.jsx
@@ -25,19 +25,20 @@ export default function App() {
   const currentWord = sentenceArray[allWordCount];
   const noErrorInWord = !wordErrorIndex.includes(allWordCount);
 
+  const fetchData = async () => {
+    try {
+      const response = await fetch(
+        "https://random-word-api.vercel.app/api?words=100&length=5"
+      );
+      const data = await response.json();
+      setSentence(data.join(" "));
+      setLoading(false);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
   useEffect(() => {
-    const fetchData = async () => {
-      try {
-        const response = await fetch(
-          "https://random-word-api.vercel.app/api?words=100"
-        );
-        const data = await response.json();
-        setSentence(data.join(" "));
-        setLoading(false);
-      } catch (error) {
-        console.log(error);
-      }
-    };
     fetchData();
   }, []);
 
@@ -201,11 +202,13 @@ export default function App() {
   };
 
   const newTest = () => {
+    // Reset the state when a test is reset.
+    setLoading(true);
     setTypedKey("");
     setTrackKeyIndex(1);
     setErrorIndexes([]);
     setSpaceErrorIndexes([]);
-    // setSentence("here is a new sentence to get started on ");
+    fetchData();
     setCorrectWordCount(0);
     setAllWordCount(0);
     setTrackKeyCount([0]);
@@ -219,7 +222,7 @@ export default function App() {
         <img className="logo-icon" src="/keyboard.svg" alt="keyboard" />
         <div className="logo-text">Key Sprint</div>
       </div>
-      {!endOfTest ? (
+      {!endOfTest && !loading ? (
         <div className="text-container">
           <Timer handleEndOfTest={handleEndOfTest} />
           <Sentence
