4280d9ca6108a6adf7b859f88c9c50e9705e7f85
Jesse McDermott <mcdermott.jess9@gmail.com>
2024-05-07 17:30:51 -0700

    Handle auto scroll
    Ensure that the sentence scrolls down when neccessary.
    
    Refacotr the handleKeyPress function.

--- src/Sentence.jsx
+++ src/Sentence.jsx
@@ -1,11 +1,38 @@
+import React, { useEffect, useRef } from "react";
+
 export default function Sentence({
   sentence,
   errorIndexes,
   spaceErrorIndexes,
   typedKey,
+  allWordCount,
 }) {
+  const containerRef = useRef(null);
+  const wordCountForScroll = useRef(0);
+  const prevAllWordCount = useRef(allWordCount);
+
+  // Handle auto scroll.
+  useEffect(() => {
+    if (allWordCount > prevAllWordCount.current && allWordCount > 12) {
+      wordCountForScroll.current++;
+    }
+
+    if (allWordCount < prevAllWordCount.current && allWordCount < 80) {
+      wordCountForScroll.current = Math.max(0, wordCountForScroll.current - 1);
+      const container = containerRef.current;
+      container.scrollTop -= 4;
+    }
+    prevAllWordCount.current = allWordCount;
+
+    if (wordCountForScroll.current === 13) {
+      const container = containerRef.current;
+      container.scrollTop += allWordCount;
+      wordCountForScroll.current = 1;
+    }
+  }, [allWordCount]);
+
   return (
-    <div className="main-text">
+    <div className="main-text" ref={containerRef}>
       {sentence.split("").map((letter, index) => {
         const isError = errorIndexes.includes(index);
         const isSpaceError = spaceErrorIndexes.includes(index);
